707469b6343c4a6a6a701886c705ff392077cb46
TAKAISHI Saya <21716056@jwu.ac.jp>
2019-10-07 15:59:29 +0900

    counter

---
+++ counter.py
@@ -0,0 +1,19 @@
+class Counter(object):
+  def __init__(self):
+      self.cnt = 0
+  def count(self):
+      self.cnt += 1
+  def doublecount(self):
+      self.cnt += 2
+  def reset(self):
+      self.cnt =0
+  def show(self):
+      print(self.cnt)
+  def __repr__(self):
+      return str(self.cnt)
+c=Counter()
+c.show()
+c.doublecount()
+c.show()
+print(type(c))
+print(c)

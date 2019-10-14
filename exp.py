87c0c7054a407f1f17a86dffc8c7d3ece0287b7e
TAKAISHI Saya <21716056@jwu.ac.jp>
2019-10-14 16:44:35 +0900

    exp.py3

--- exp.py
+++ exp.py
@@ -18,22 +18,24 @@ assert isinstance(v, Expr) # ==> True
 assert isinstance(v, Val) # ==> True
 assert not isinstance(v, int) 
 
+
 class Add(Expr):
     __slots__=['left', 'right']
     def __init__(self, a, b):
-        self.left = a   # aとb は式
+        if not isinstance(a, Expr):
+            a = Val(a)
+        if not isinstance(b, Expr):
+            b = Val(b)
+        self.left = a
         self.right = b
     def eval(self):
         return self.left.eval() + self.right.eval()
 
-e = Add(Val(1), Val(2))  # 1+2
-assert e.eval() == 3
 
-e = Add(1,2)
-assert e.eval() == 3
+e = Add(1, Add(1,2))
+print(e.eval())
+assert e.eval() == 4
 
 
-e = Add(Val(1),Add(Val(2),Val(3)))
-assert e.eval() == 6
 
 

068c00a625bf288246ab3c5007a73388677a0b8c
TAKAISHI Saya <21716056@jwu.ac.jp>
2019-10-28 16:36:18 +0900

    paster4

--- paster.py
+++ paster.py
@@ -1,13 +1,18 @@
 from exp import Val, Add, Sub, Mul, Div
 
 def parse(s: str):
-    pos = s.find('+')
-    if pos == -1:
-        return Val(int(s))
-    else:
+    if s.find('+') > 0:
+        pos = s.find('+')
+        s1 = s[0:pos]
+        s2 = s[pois+1:]
+        return Mul(parse(s1), parse(s2))
+    if s.find('+') > 0:
+        pos = s.find('+')
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Add(parse(s1)), parse(s2))
+    return Val(int(s))
 
 e = parse("1+2+3")
 print(e, e.eval())
+assert e.eval() == -4

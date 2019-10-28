7cac9eebb6491f951dcca159f9f2eef12fd984d6
TAKAISHI Saya <21716056@jwu.ac.jp>
2019-10-28 16:25:08 +0900

    paster3

--- paster.py
+++ paster.py
@@ -1,30 +1,13 @@
-from exp import Val, Add
-
-def parse(s: str):
-    num = int(s)
-    return Val(num)
-
-e = parse("123")
-print(e)
-
-s = "123+456"
-pos = s.find('+')
-print('pos',pos)
-
-s1 = s[0:pos]
-s2 = s[pos+1:]
-print(s, s1, s2)
-'''
+from exp import Val, Add, Sub, Mul, Div
 
 def parse(s: str):
     pos = s.find('+')
     if pos == -1:
-        num = int(s)
-        return Val(num)
+        return Val(int(s))
     else:
         s1 = s[0:pos]
         s2 = s[pos+1:]
-        return Add(Val(int(s1)), Val(int(s2)))
+        return Add(parse(s1)), parse(s2))
 
-e = parse("123+456+789")
-print(e)
+e = parse("1+2+3")
+print(e, e.eval())

60595190fdf4add0c4af7041af578d315333534d
TAKAISHI Saya <21716056@jwu.ac.jp>
2019-11-25 16:32:29 +0900

    icalc3

--- icalc.py
+++ icalc.py
@@ -1,12 +1,26 @@
 import pegpy
 peg = pegpy.grammar('''
-Expression = Product (^{ '+' Product #Add })*
-Product = Value (^{ '*' Value #Mul })*
-Value = { [0-9]+ #Int }
+Expressin = Product (^{'+' Product #Add})*
+Product = Value (^{'*' Value #Mul})*
+Value = {[0-9]+#Int}
 ''')
 parser = pegpy.generate(peg)
-t = parser('1+2*3')
-print(repr(t))
-t = parser('@2')
-print(repr(t))
-
+def calc(t)
+    if t == 'Int':
+        return int(str(t))
+    if t == 'Add':
+        return calc(t[0]) + calc(t[1])
+    if t == 'Mul':
+        return calc(t[0]) * calc(t[1])
+    print(f'TODO {t.tag}')
+    return 0
+    #t = parser('1+2*3+4*5)
+    #print(reper(t))
+    #print(calc(t))
+    def main():
+        s = input('$')
+        t = parser(s)
+        print(calc(t))
+    if __name__ == '__main__':
+                main()
+                

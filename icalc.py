c3d632c2b1b6526913e23302a544ebbca13e424c
TAKAISHI Saya <21716056@jwu.ac.jp>
2019-11-25 16:22:00 +0900

    icalc2

--- icalc.py
+++ icalc.py
@@ -1,6 +1,12 @@
-example Expr (1+2)*3
-example Expr 1+2*3
-Expr = { Prod '+' Prod #Add } / Prod
-Prod = { Value '*' Value #Mul } / Value
-Value = { DIGIT+ #Int } / '(' Expr ')' 
-DIGIT = [0-9]
+import pegpy
+peg = pegpy.grammar('''
+Expression = Product (^{ '+' Product #Add })*
+Product = Value (^{ '*' Value #Mul })*
+Value = { [0-9]+ #Int }
+''')
+parser = pegpy.generate(peg)
+t = parser('1+2*3')
+print(repr(t))
+t = parser('@2')
+print(repr(t))
+
